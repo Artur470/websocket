ab0c0d92e00cbb2edf16e19bcad9d68d8592d523
Artur <arturunkulov30@gmail.com>
2024-12-14 17:01:43 +0600

    first commit

---
+++ config/asgi.py
@@ -0,0 +1,17 @@
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from chat import routing  # Импортируем routing.py из chat
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns  # Используем маршруты WebSocket из routing.py
+        )
+    ),
+})
